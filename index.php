542f733a1abe74c5d810ccc743b442389afdd1c3
Yogeshkad <4671476+Yogeshkad@users.noreply.github.com>
2017-07-21 13:14:53 +0200

    comment box added

--- index.php
+++ index.php
@@ -9,7 +9,14 @@
   	
 				get_template_part( 'content', get_post_format() );
   
-			endwhile; endif; 
+			endwhile;?>
+			<nav>
+	<ul class="pager">
+		<li><?php next_posts_link( 'Previous' ); ?></li>
+		<li><?php previous_posts_link( 'Next' ); ?></li>
+	</ul>
+</nav>
+			<?php endif; 
 			?>
 
 		</div> <!-- /.blog-main -->
